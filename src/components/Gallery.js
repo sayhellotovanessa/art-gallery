2632cf06c8c9efad6107972b637c645a80bddd4a
Guedes, Vanessa <vanessa.guedes@accenture.com>
2020-08-13 21:35:22 +0200

    Adds flickity image library to the project

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import flickity from 'flickity'
 
 function Gallery() {
+    const array = ["https://cdn.pixabay.com/photo/2020/07/09/09/05/cat-5386372_960_720.jpg", "https://cdn.pixabay.com/photo/2017/07/25/01/22/cat-2536662_960_720.jpg", "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg"];
+    const images = array.map(image => { 
+           return <div class="gallery-cell">
+           <img src={image} class="img-responsive" /></div>
+    });
+
+
     return (
-    <div class="gallery">
-                Gallery arry arry
+    <div class="gallery js-flickity" data-flickity='{ "cellAlign": "right", "contain": true }'>
+     { images }
     </div>
 
     )
--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ import App from './components/App'
 ReactDOM.render(
 <App></App>, 
 
-  document.getElementById('root'))
+document.getElementById('root'))
