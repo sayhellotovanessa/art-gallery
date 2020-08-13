176b347eb6bd95e652955a90b194400842e2702f
Guedes, Vanessa <vanessa.guedes@accenture.com>
2020-08-13 22:00:33 +0200

    Increases image widths from 500 to 750px

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,7 +2,7 @@ import React from 'react'
 import flickity from 'flickity'
 
 function Gallery() {
-    const array = ["https://source.unsplash.com/500x500/?monet", "https://source.unsplash.com/500x500/?nasa", "https://source.unsplash.com/500x500/?magritte", "https://source.unsplash.com/500x500/?louvre", "https://source.unsplash.com/500x500/?streetart", "https://source.unsplash.com/500x500/?grafitti"];
+    const array = ["https://source.unsplash.com/780x500/?monet", "https://source.unsplash.com/780x500/?nasa", "https://source.unsplash.com/780x500/?magritte", "https://source.unsplash.com/780x500/?louvre", "https://source.unsplash.com/780x500/?streetart", "https://source.unsplash.com/780x500/?grafitti"];
     const images = array.map(image => { 
            return <div class="gallery-cell">
            <img  data-flickity-lazyload={image} class="img-responsive" /></div>
